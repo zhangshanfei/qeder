2e667c98e5dab136e51abbf3639c35059587e1c1
zhangshanfei <zhsf20@163.com>
2017-10-10 19:12:17 +0800

    修改获取post参数

--- web/savescore.php
+++ web/savescore.php
@@ -2,6 +2,8 @@
 /* developer 张善飞 QQ3460698227
 *
 */
+$Uemail = explode(',',$_POST)[0];
+$Uscore = explode(',',$_POST)[1];
 	$mysqli = new mysqli('127.0.0.1','root','root','qeder');
 	
 	$sqlstr = "INSERT INTO qe_gamescore(useremail,score)value('$Uemail','$Uscore')";

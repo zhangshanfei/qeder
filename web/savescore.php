7bcfac017df5dfb21fcf4f035714370b4fb010e0
zhangshanfei <zhsf20@163.com>
2017-10-10 19:11:12 +0800

    修复错误，写入数据库表。（可能是之前改了，git操作错误没提交，本地也没保存 -_-!）

--- web/savescore.php
+++ web/savescore.php
@@ -3,12 +3,7 @@
 *
 */
 	$mysqli = new mysqli('127.0.0.1','root','root','qeder');
-	/* if ($mysqli->connect_error) {
-		die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
-	}
-	elseif(mysqli_connect_error()) {
-		die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-	} */
+	
 	$sqlstr = "INSERT INTO qe_gamescore(useremail,score)value('$Uemail','$Uscore')";
 	$result = $mysqli->query($sqlstr);
 	$mysqli->autocommit(TRUE);

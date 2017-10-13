541d51504eb02f667f0d822ff1fd2d9e22254330
zhangshanfei <zhsf20@163.com>
2017-10-13 19:42:41 +0800

    修改每个用户存储的游戏次数为三次

--- web/savescore.php
+++ web/savescore.php
@@ -7,7 +7,9 @@ $postStr = file_get_contents("php://input");
 $Uemail = explode('&',$postStr)[0];
 $Uscore = explode('&',$postStr)[1];
 $mysqli = new mysqli('localhost','root','root','qeder');
-$sqlstr = "INSERT INTO qe_gamescore(useremail,score)value('$Uemail','$Uscore');";
-$result = $mysqli->query($sqlstr);
+$sqlstr = "drop procedure if exists ins_sc;  create procedure ins_sc()  begin set @tot =(select count(id)from qe_gamescore where useremail= '$Uemail' ) ;if (@tot<3) then  insert into qe_gamescore values(null, '$Uemail' , '$Uscore'); end if;  end;  call ins_sc();  ";
+$result = $mysqli->multi_query($sqlstr);
 $mysqli->autocommit(TRUE);
 $mysqli->close();
+
+
